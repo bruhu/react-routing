016bd99fe2160b98f23a2b6077af14b12502c439
Bru <annabru.abh@gmail.com>
2019-04-23 10:56:07 +0200

    Added list to Navbar, imported it to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <Navbar />
       </header>
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
     <div>
-      
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Info</li>
+        <li>Credits</li>
+      </ul>
     </div>
-  )
+  );
 }
