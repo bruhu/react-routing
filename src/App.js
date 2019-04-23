43ddbd7056dae3d7572581e0ff1303cb58bc400b
Bru <annabru.abh@gmail.com>
2019-04-23 11:30:40 +0200

    added content to components and routesssss

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Info from "./Components/Info";
+import Credits from "./Components/Credits";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Components/Home";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <div>
             <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/info" component={Info} />
+            <Route path="/credt" component={Credits} />
           </div>
         </header>
       </div>
--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Info() {
   return (
     <div>
-      
+      <p>Info</p>
     </div>
-  )
+  );
 }
