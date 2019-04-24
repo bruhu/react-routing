b7c05cd2ae3aeeb5ab58bcfbb48de753420e0e3a
Bru <annabru.abh@gmail.com>
2019-04-24 10:48:31 +0200

    it was BrowserRouter instead of Router, dah

--- src/App.js
+++ src/App.js
@@ -9,31 +9,111 @@ import Home from "./Components/Home";
 import Topics from "./Components/Topics";
 import NotFound from "./Components/NotFound";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink
+} from "react-router-dom";
 
-function App() {
+const routes = [
+  {
+    path: "/home",
+    exact: true,
+    render: "<h2>Home</h2> <p>This is the homepage of this SPA.</p>"
+  },
+  {
+    path: "/about",
+    exact: true,
+    render: "<h2>About</h2><p>It's a me.</p>"
+  },
+  {
+    path: "/info",
+    exact: true,
+    render: "<h2>Info</h2><p>Lorem mierdas.</p>"
+  },
+  {
+    path: "/credits",
+    exact: true,
+    render: "<h2>Credits</h2><p>Thanks Mom.</p>"
+  },
+  {
+    path: "/topics",
+    exact: true,
+    render: "<h2>Topics</h2><p>Here we can select some topics.</p>"
+  }
+];
+
+function NavbarExample() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div>
-            <Navbar />
-            <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/info" component={Info} />
-            <Route path="/credits" component={Credits} />
-            {/* <Route path="/topics" render={() => <h1>Topics</h1>} /> */}
-            <Route path="/topics" component={Topics} />
-            <Route path="/whatever" component={Topics} />
-            <Route component={NotFound} />
-            </Switch>
-          </div>
-        </header>
+      <div>
+        <ul className="navbar-list">
+          <li>
+            <NavLink to="/home" activeClassName="selected-navbar">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeClassName="selected-navbar">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/info" activeClassName="selected-navbar">
+              Info
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/credits" activeClassName="selected-navbar">
+              Credits
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/topics" activeClassName="selected-navbar">
+              Topics
+            </NavLink>
+          </li>
+        </ul>
+
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.sidebar}
+          />
+        ))}
       </div>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default NavbarExample;
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <div>
+//             <Navbar />
+//             <Switch>
+//             <Route path="/home" component={Home} />
+//             <Route path="/about" component={About} />
+//             <Route path="/info" component={Info} />
+//             <Route path="/credits" component={Credits} />
+//             {/* <Route path="/topics" render={() => <h1>Topics</h1>} /> */}
+//             <Route path="/topics" component={Topics} />
+//             <Route path="/whatever" component={Topics} />
+//             <Route component={NotFound} />
+//             </Switch>
+//           </div>
+//         </header>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
